ca2705de06263686ee250adbe5d75fa1b7471a5b
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-04-21 17:02:34 +0300

    Clean up

--- library/Fx.hs
+++ library/Fx.hs
@@ -1,8 +1,9 @@
 module Fx
 (
   BothEffects,
-  Executor(..),
   executorOfBothEffects,
+  Executor(..),
+  runExecutor,
 )
 where
 
@@ -16,20 +17,6 @@ newtype BothEffects effect1 effect2 context result =
   BothEffects (ReaderT (Executor effect1 context, Executor effect2 context) context result)
   deriving (Functor, Applicative, Monad, MonadIO)
 
-runBothEffects
-  :: BothEffects effect1 effect2 context result
-  -> Executor effect1 context
-  -> Executor effect2 context
-  -> context result
-runBothEffects (BothEffects reader) executor1 executor2 =
-  runReaderT reader (executor1, executor2)
-
-{-|
-Executor of a single effect.
--}
-newtype Executor effect context =
-  Executor { runExecutor :: forall result. effect result -> context result }
-
 {-|
 Composes the executors of each effect into an executor of both.
 -}
@@ -39,3 +26,16 @@ executorOfBothEffects
   -> Executor (BothEffects effect1 effect2 context) context
 executorOfBothEffects executor1 executor2 =
   Executor (\(BothEffects reader) -> runReaderT reader (executor1, executor2))
+
+{-|
+Executor of a single effect.
+-}
+newtype Executor effect context =
+  Executor (forall result. effect result -> context result)
+
+{-|
+Use the executor to run an effect in a context.
+-}
+runExecutor :: Executor effect context -> effect result -> context result
+runExecutor (Executor def) =
+  def
