9de8c7cbf28b9a18275c75c3e2edf92f73aa8729
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-04-22 17:22:39 +0300

    Reorganize

---
+++ library/Fx/Executor.hs
@@ -0,0 +1,38 @@
+module Fx.Executor
+(
+  Executor(..),
+  execute,
+  mapEffect,
+  mapContext,
+)
+where
+
+import Fx.Prelude
+
+
+{-|
+An abstraction over the execution of @effect@ in @context@, both producing the same @result@.
+-}
+newtype Executor effect context =
+  Executor (forall result. effect result -> context result)
+
+{-|
+Use the executor to run an effect in a context.
+-}
+execute :: effect result -> Executor effect context -> context result
+execute effect (Executor def) =
+  def effect
+
+{-|
+Map the effect part of an executor (contravariantly).
+-}
+mapEffect :: (forall result. newEffect result -> oldEffect result) -> Executor oldEffect context -> Executor newEffect context
+mapEffect mapping (Executor fn) =
+  Executor (fn . mapping)
+
+{-|
+Map the context part of an executor.
+-}
+mapContext :: (forall result. oldContext result -> newContext result) -> Executor effect oldContext -> Executor effect newContext
+mapContext mapping (Executor fn) =
+  Executor (mapping . fn)
