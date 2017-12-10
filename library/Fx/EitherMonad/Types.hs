d47f1918ac5e64160f22790405d9107ea240b7e0
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-12-10 14:13:14 +0300

    Wrap the transformation type and rename the module

--- library/Fx/EitherMonad/Types.hs
+++ library/Fx/EitherMonad/Types.hs
@@ -7,7 +7,7 @@ import Fx.Prelude
 A sum of two monads.
 Allows for horizontal composition of monads instead of nesting as with monad transformers.
 
-To execute it use 'Fx.Transformation.eitherMonad'.
+To execute it use 'Fx.Transform.eitherMonad'.
 -}
 newtype EitherMonad leftMonad rightMonad result =
   EitherMonad (forall context. Monad context => (forall x. leftMonad x -> context x) -> (forall x. rightMonad x -> context x) -> context result)
--- library/Fx/Transform.hs
+++ library/Fx/Transform.hs
@@ -1,4 +1,4 @@
-module Fx.Transformation
+module Fx.Transform
 where
 
 import Fx.Prelude
@@ -9,21 +9,21 @@ import Fx.EitherMonad.Types
 Natural transformation.
 An abstraction over the transformation of kind-2 @input@ to @output@, both producing the same @result@.
 -}
-type input --> output =
-  forall result. input result -> output result
+newtype input --> output =
+  Transform (forall result. input result -> output result)
 
 {-|
 Given a natural transformation of the left monad and a natural transformation of the right monad,
 produces a natural transformation of either of them.
 -}
 eitherMonad :: Monad either => (left --> either) -> (right --> either) -> (EitherMonad left right --> either)
-eitherMonad leftTrans rightTrans (EitherMonad context) =
-  context leftTrans rightTrans
+eitherMonad (Transform leftTrans) (Transform rightTrans) =
+  Transform (\ (EitherMonad context) -> context leftTrans rightTrans)
 
 mapInput :: (newInput --> oldInput) -> (oldInput --> output) -> (newInput --> output)
-mapInput mapping trans =
-  trans . mapping
+mapInput (Transform mapping) (Transform trans) =
+  Transform (trans . mapping)
 
 mapOutput :: (oldOutput --> newOutput) -> (input --> oldOutput) -> (input --> newOutput)
-mapOutput mapping trans =
-  mapping . trans
+mapOutput (Transform mapping) (Transform trans) =
+  Transform (mapping . trans)
